390364a42eaffd089b23386fee2058d2fec2dc47
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-21 00:31:29 +0200

    Refactor to implement Square class and remove MagicClass
    Removed MagicClass and added Square class with size attribute.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Defines MagicClass."""
-
-import math
+"""Module that defines a Square class."""
 
 
-class MagicClass:
-    """Defines MagicClass."""
+class Square:
+    """Defines a square."""
 
-    def __init__(self, radius=0):
-        if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
-        self.__radius = radius
-
-    def area(self):
-        return (self.__radius ** 2) * math.pi
-
-    def circumference(self):
-        return 2 * math.pi * self.__radius
+    def __init__(self, size):
+        self.__size = size
