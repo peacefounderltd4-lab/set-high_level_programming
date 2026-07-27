faff511744707627515f83324e300c0bad91cfd4
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 06:04:42 -0700

    Refactor loop to print alphabet excluding 'e' and 'q'

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('e') and letter != ord('q'):
-        print("{:c}".format(letter), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
