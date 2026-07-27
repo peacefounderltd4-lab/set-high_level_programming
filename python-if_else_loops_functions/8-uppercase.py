53f0cdfc8b63398e5048c4fcea5690861dcd170d
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 06:07:38 -0700

    Refactor uppercase function to use ord for case conversion

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    for char in str:
-        if 'a' <= char <= 'z':
-            print("{:c}".format(ord(char) - 32), end="")
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{:c}".format(ord(c) - 32), end="")
         else:
-            print("{:c}".format(ord(char)), end="")
+            print("{:c}".format(ord(c)), end="")
     print()
