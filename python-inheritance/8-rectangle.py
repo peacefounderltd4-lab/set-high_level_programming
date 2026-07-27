55dba1280158dfc60073f7007cf2bc10f10a133d
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 04:33:28 -0700

    Add Rectangle class with validation for dimensions

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing the Rectangle class."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
