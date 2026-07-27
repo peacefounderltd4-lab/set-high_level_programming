9da78aebcd3d007ec8b39f415743f525a3930034
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 06:16:58 -0700

    Refactor character printing in 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-for letter in range(ord('z'), ord('a') - 1, -1):
-    if (ord('z') - letter) % 2 == 0:
-        print("{:c}".format(letter), end="")
-    else:
-        print("{:c}".format(letter - 32), end="")
+for i in range(122, 96, -1):
+    print("{:c}".format(i if (122 - i) % 2 == 0 else i - 32), end="")
