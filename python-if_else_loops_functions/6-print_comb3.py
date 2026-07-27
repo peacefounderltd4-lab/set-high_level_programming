594b28cb513dfa5023bd3f9a61c4e5a7e7508dd9
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 06:06:27 -0700

    Refactor print statements in print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:02d}".format(i * 10 + j))
+            print("{}{}".format(i, j))
         else:
-            print("{:02d}".format(i * 10 + j), end=", ")
+            print("{}{}".format(i, j), end=", ")
