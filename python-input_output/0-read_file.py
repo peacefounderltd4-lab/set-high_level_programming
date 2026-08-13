090100f7ad24e90230058a61f2c6f99711fdcedc
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-13 21:53:13 +0200

    Fix indentation in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module to read a text file."""
+
+
 def read_file(filename=""):
+    """Read a text file and print its contents to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
