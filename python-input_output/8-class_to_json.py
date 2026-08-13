1ea3d643e722087cf0bded47363eaa6e84fb3c5b
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-13 22:25:56 +0200

    Update 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module for converting a class instance to a dictionary."""
+
+
 def class_to_json(obj):
+    """Return the dictionary representation of a class instance."""
     return obj.__dict__
