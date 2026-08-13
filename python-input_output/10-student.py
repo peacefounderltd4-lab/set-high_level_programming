34d92798928e8c4c663cd065f55fb977681830eb
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-13 22:28:38 +0200

    Refactor to_json method for better clarity

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""Defines a Student class."""
+
+
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
-            return {
-                key: getattr(self, key)
-                for key in attrs
-                if hasattr(self, key)
-            }
-        return self.__dict__
+        """Return the dictionary representation of the Student."""
+        if attrs is None:
+            return self.__dict__
+
+        return {
+            key: getattr(self, key)
+            for key in attrs
+            if key in self.__dict__
+        }
