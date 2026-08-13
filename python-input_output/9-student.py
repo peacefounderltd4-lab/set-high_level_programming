5bb5b95d823a73fa1fd8328bb1d63f1d7de1d97b
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-13 22:27:08 +0200

    Enhance to_json method with attrs parameter
    Add optional attributes filtering to to_json method.

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
+"""Defines a Student class."""
+
+
 class Student:
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the Student."""
+        if attrs is None:
+            return self.__dict__
+
+        return {
+            key: getattr(self, key)
+            for key in attrs
+            if key in self.__dict__
+        }
