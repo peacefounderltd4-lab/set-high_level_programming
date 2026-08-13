79f870f0c939598f5a437fe22bcef90063146c3d
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-13 22:06:22 +0200

    Refactor load_from_json_file to use 'r' mode
    Updated file opening mode to 'r' for clarity.

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Module for loading an object from a JSON file."""
+
 import json
 
 
 def load_from_json_file(filename):
-    with open(filename, encoding="utf-8") as f:
+    """Create a Python object from a JSON file."""
+    with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
