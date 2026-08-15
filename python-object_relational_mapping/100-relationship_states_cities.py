0d03ad5931ad42cdcffd93eb54f76f2fa0196317
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-15 11:58:28 +0200

    Add script to create California and San Francisco
    This script creates a state 'California' and a city 'San Francisco' in a MySQL database using SQLAlchemy.

---
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Create California and San Francisco."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="California")
+    city = City(name="San Francisco")
+    state.cities.append(city)
+
+    session.add(state)
+    session.commit()
+
+    session.close()
