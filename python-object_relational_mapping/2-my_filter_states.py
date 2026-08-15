c1fac6a59f458197ab7143f028d4ff5f492f86fd
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-15 12:18:40 +0200

    Refactor variable usage for database connection

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-"""Filter states by user input."""
+"""Lists all states matching a user-provided name."""
+
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
     cursor = db.cursor()
 
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
-        sys.argv[4]
+        state_name
     )
 
     cursor.execute(query)
 
-    for row in cursor.fetchall():
-        print(row)
+    for state in cursor.fetchall():
+        print(state)
 
     cursor.close()
     db.close()
