27b90c2e479b43fb389fde4771b0e3c74d1ac9f8
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-15 11:50:33 +0200

    Add script to safely filter states by user input
    This script connects to a MySQL database and safely filters states based on user input.

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Safely filter states by user input."""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+        (sys.argv[4],)
+    )
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    db.close()
