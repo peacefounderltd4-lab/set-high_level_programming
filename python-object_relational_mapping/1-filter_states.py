e9c5c5ced6d71a32d3b5341eb3788e2badaa88d8
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-16 16:55:41 +0200

    Filter states by name starting with 'N'
    Updated the script to list states starting with 'N' and simplified the database connection parameters.

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
-"""Lists all states from the database that match a given name."""
-
-import sys
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
+import sys
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-
     cursor = db.cursor()
     cursor.execute(
-        "SELECT id, name FROM states WHERE name = %s "
-        "ORDER BY id ASC",
-        (state_name,)
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     )
-
-    for state in cursor.fetchall():
-        print(state)
-
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
     cursor.close()
     db.close()
