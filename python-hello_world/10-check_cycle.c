4818fb37bbe1e4b8d102842c91b2dab7b8f2e00c
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-22 14:18:12 +0200

    Add cycle detection function for linked list
    Implement function to detect cycle in linked list

---
+++ python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to the head of the list
+ * Return: 1 if the list has a cycle, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	if (list == NULL)
+		return (0);
+
+	slow = list;
+	fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
