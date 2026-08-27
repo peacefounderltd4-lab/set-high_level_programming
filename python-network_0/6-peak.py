a62310b41144621edc1a9104df882c6c819899a6
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 10:46:16 +0200

    Add function to find a peak in a list of integers
    Implement a binary search algorithm to find a peak in a list of unsorted integers.

---
+++ python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Find a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Find a peak using binary search."""
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
