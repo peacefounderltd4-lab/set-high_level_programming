90479256f05f8267c15cd4e91fa508d931b3c395
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:17:45 +0200

    Modify database connection and add comments
    Updated the database connection string and improved comments.

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""
-Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
-"""
+"""Add Louisiana to the states table."""
+
 import sys
-from model_state import Base, State
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from model_state import Base, State
+
+
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
-    print("{}".format(new_state.id))
+
+    print(new_state.id)
+
     session.close()
