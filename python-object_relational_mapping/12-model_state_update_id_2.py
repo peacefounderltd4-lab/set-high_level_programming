0a79bd97e1d484e8c816bfec1489a94804c2be98
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:18:35 +0200

    Modify state name to 'New Mexico' in database
    Updated the script to change the name of the state with id 2 to 'New Mexico'.

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa.
-"""
+"""Change the name of state with id 2."""
+
 import sys
-from model_state import Base, State
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from model_state import Base, State
+
+
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
     state = session.query(State).filter(State.id == 2).first()
-    if state:
+
+    if state is not None:
         state.name = "New Mexico"
         session.commit()
+
     session.close()
