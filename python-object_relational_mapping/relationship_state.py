9967e5ddd77fb1b6862f9f9d79fef2c70df30318
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:25:32 +0200

    Create State model with cities relationship
    Define the State model with a relationship to City using SQLAlchemy.

---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define the State model with a relationship to City."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represent a state."""
+
+    __tablename__ = "states"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        "City",
+        back_populates="state",
+        cascade="all, delete-orphan"
+    )
