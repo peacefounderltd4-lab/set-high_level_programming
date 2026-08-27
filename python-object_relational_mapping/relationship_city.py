0ed009cd6a9b19f6ef32e48182141c33531f8743
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:50:15 +0200

    Refactor City model in relationship_city.py
    Refactor City model for improved readability and structure.

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""City model linked to State."""
+"""Define City and its relationship with State."""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+
+from relationship_state import Base
 
 
 class City(Base):
-    """Represents a city in the cities table."""
+    """Represent a city."""
 
     __tablename__ = "cities"
 
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
 
-    state = relationship("State", back_populates="cities")
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
+
+    state = relationship(
+        "State",
+        back_populates="cities"
+    )
