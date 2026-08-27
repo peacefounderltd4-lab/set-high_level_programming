59ffb76c38ca793f09d76e8065503fa9f4e2e377
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 10:40:58 +0200

    Fix curl command options for body retrieval

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -s -L -w "%{http_code}" -o /tmp/body "$1" | grep -q '^200$' && cat /tmp/body
+curl -s -o /tmp/body -w "%{http_code}" "$1" | grep -q "^200$" && cat /tmp/body
 rm -f /tmp/body
