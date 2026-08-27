e163e1603fc9b137bcee4e96392c6b6657b6ecb5
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:42:16 +0200

    Refactor SQLAlchemy query and improve readability

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""List all states and their cities using one query."""
+"""List all states and their cities using one SQLAlchemy query."""
 
 import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, joinedload
 
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             sys.argv[1],
             sys.argv[2],
             sys.argv[3]
@@ -23,14 +23,17 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).options(
-        joinedload(State.cities)
-    ).order_by(State.id).all()
+    states = (
+        session.query(State)
+        .options(joinedload(State.cities))
+        .order_by(State.id)
+        .all()
+    )
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
-        for city in sorted(state.cities, key=lambda c: c.id):
+        for city in sorted(state.cities, key=lambda city: city.id):
             print("\t{}: {}".format(city.id, city.name))
 
     session.close()
