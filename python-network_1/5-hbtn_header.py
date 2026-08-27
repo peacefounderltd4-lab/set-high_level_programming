80b7d6f76f3659703b5b61ab61448d64915468eb
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 08:44:35 +0200

    Add script to display X-Request-Id header

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Display the X-Request-Id header using requests."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
