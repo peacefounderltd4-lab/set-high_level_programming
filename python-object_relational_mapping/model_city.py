e923b566eb2b42ac2c6ce90a3707b8fa9be11add
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:20:45 +0200

    Refactor City class docstrings and state_id definition
    Updated docstrings for clarity and formatted state_id column.

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""
-Defines a City class that links to the MySQL table cities.
-"""
-from model_state import Base
+"""Define the City model."""
+
 from sqlalchemy import Column, Integer, String, ForeignKey
+
+from model_state import Base
 
 
 class City(Base):
-    """
-    City class attributes: id, name, state_id.
-    """
-    __tablename__ = 'cities'
+    """Represent a city."""
+
+    __tablename__ = "cities"
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(
+        Integer,
+        ForeignKey("states.id"),
+        nullable=False
+    )
