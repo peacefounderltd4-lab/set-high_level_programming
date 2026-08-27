86f88ea2cfcced33ea7502f2e5d685061b502a56
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-27 07:16:19 +0200

    Improve readability and update database connection
    Refactor code for better readability and update database connection string.

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as argument from hbtn_0e_6_usa.
-"""
+"""Get a State by name."""
+
 import sys
-from model_state import Base, State
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from model_state import Base, State
+
+
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(
+        State.name == sys.argv[4]
+    ).first()
+
     if state is None:
         print("Not found")
     else:
-        print("{}".format(state.id))
+        print(state.id)
+
     session.close()
