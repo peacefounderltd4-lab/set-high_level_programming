8afe211cd1d0f0d0b964004f126b60fc7b118484
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-09-08 23:35:54 +0200

    Replace database script with number printing loop
    Replaced database connection script with a loop to print numbers from 0 to 99 formatted with leading zeros.

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,31 +1,6 @@
 #!/usr/bin/python3
-"""
-Script isoma no kwereka ama state hamwe n'imijyi iyishamikiyeho
-uhereye kuri database hbtn_0e_4_usa
-"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
-
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    states = session.query(State).order_by(State.id).all()
-
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-        for city in sorted(state.cities, key=lambda x: x.id):
-            print("    {}: {}".format(city.id, city.name))
-
-    session.close()
-zd
+for i in range(100):
+    if i < 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
