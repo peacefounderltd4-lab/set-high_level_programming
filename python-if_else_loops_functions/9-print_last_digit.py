709f779ddcdb9c4059f3fd3a3b596a645a46f22b
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-09-09 00:46:37 +0200

    Implement print_last_digit function
    Added a function to print and return the last digit of a number.

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,5 @@
-git add 6-concat.py
-git commit -m "Fix 6-concat.py"
-git push
+#!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = abs(number) % 10
+    print(f"{last_digit}", end="")
+    return last_digit
