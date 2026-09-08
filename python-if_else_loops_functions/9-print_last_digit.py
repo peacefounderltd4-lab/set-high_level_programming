aaa2b61ad10bfe0d344c797296c4743dc80d1838
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-09-08 23:33:38 +0200

    Replace print_last_digit with state and city display script
    Replaced print_last_digit function with a script to display states and their cities from a MySQL database.

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print("{}".format(last_digit), end="")
-    return last_digit
+"""
+Script isoma no kwereka ama state hamwe n'imijyi iyishamikiyeho
+uhereye kuri database hbtn_0e_4_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in sorted(state.cities, key=lambda x: x.id):
+            print("    {}: {}".format(city.id, city.name))
+
+    session.close()
+zd
