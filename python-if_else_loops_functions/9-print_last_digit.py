40421da6f91da4ce882f08e1e1b399b1eab88926
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-09-08 23:51:36 +0200

    Refactor last digit print statements for clarity

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -7,11 +7,11 @@ if number < 0:
 else:
     last_digit = number % 10
 
+msg = "Last digit of {} is {} and is".format(number, last_digit)
+
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(
-        number, last_digit))
+    print("{} greater than 5".format(msg))
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("{} 0".format(msg))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(
-        number, last_digit))
+    print("{} less than 6 and not 0".format(msg))
